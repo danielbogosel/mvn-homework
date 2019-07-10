1e8be72f9b6f643503c09bd0e19ca3adcbb7e708
Bogosel Daniel <bogoseldaniel@gmail.com>
2019-07-10 21:56:15 +0300

    services

---
+++ src/main/java/ro/itschool/mvnbase/tema11/DocumentService.java
@@ -0,0 +1,13 @@
+package ro.itschool.mvnbase.tema11;
+
+import java.util.List;
+
+public class DocumentService {
+    private final List<Documents> documents;
+    DocumentService(List<Documents>document){
+        this.documents=document;
+    }
+
+}
+
+
